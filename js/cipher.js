3282855394cbe9ccb1fbfed98008a6233679c3e3
gmoraitis <moraitis.georgios@icloud.com>
2020-03-15 22:15:57 +0200

    Added cryptographic project

---
+++ js/cipher.js
@@ -0,0 +1,23 @@
+document.getElementById('msOutput').style.visibility = 'hidden';
+document.getElementById('cryptOutput').style.visibility = 'hidden';
+
+
+document.getElementById("msInput").addEventListener("input",function(e){
+    document.getElementById('msOutput').style.visibility = 'visible';
+
+    let ms = e.target.value.split('').reverse().join('n,+@,7');
+    document.getElementById('numOutput').innerText = ms; 
+})
+
+
+document.getElementById("cryptInput").addEventListener("input",function(e){
+    document.getElementById('cryptOutput').style.visibility = 'visible';
+
+    let ms = e.target.value.split('n,+@,7').reverse()
+    document.getElementById('realOutput').innerText = ms.toString(); 
+})
+
+
+
+
+
